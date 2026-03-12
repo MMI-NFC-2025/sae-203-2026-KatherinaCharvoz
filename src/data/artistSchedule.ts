d2c7e92ea5b0e49fd0bacc647ec5d6082e94a460
Katherina5Charvoz <katherina.charvoz@edu.univ-fcomte.fr>
2026-03-12 16:37:24 +0100

    fix dates et horaires

---
+++ src/data/artistSchedule.ts
@@ -0,0 +1,58 @@
+export const artistScheduleById: Record<
+    string,
+    {
+        genre: string;
+        horaire: string;
+        date: string;
+        scene: string;
+    }
+> = {
+    x9be9qq8r4wrgml: {
+        genre: "Comédie Tap",
+        horaire: "16:00",
+        date: "06 Juin 2025",
+        scene: "Grande Scène",
+    },
+    k3je02zet3l6omq: {
+        genre: "Soul Tap",
+        horaire: "18:30",
+        date: "06 Juin 2025",
+        scene: "Scène Découverte",
+    },
+    j8nriyucmzjtkim: {
+        genre: "Jeune Talent",
+        horaire: "14:00",
+        date: "06 Juin 2025",
+        scene: "Scène Découverte",
+    },
+    "5ddozlisgqo03jc": {
+        genre: "Tap Jazz",
+        horaire: "20:00",
+        date: "06 Juin 2025",
+        scene: "Grande Scène",
+    },
+    tvwnq35rr932rcr: {
+        genre: "Hip-Hop Tap",
+        horaire: "19:00",
+        date: "07 Juin 2025",
+        scene: "Scène Découverte",
+    },
+    iq6z8renidcpc7t: {
+        genre: "Soul Tap",
+        horaire: "16:00",
+        date: "07 Juin 2025",
+        scene: "Scène Découverte",
+    },
+    nm8abdkh6pknuw6: {
+        genre: "Irish Tap",
+        horaire: "20:30",
+        date: "07 Juin 2025",
+        scene: "Grande Scène",
+    },
+    bw4hcnx9yz81hra: {
+        genre: "Flamenco Tap",
+        horaire: "17:30",
+        date: "07 Juin 2025",
+        scene: "Grande Scène",
+    },
+};
